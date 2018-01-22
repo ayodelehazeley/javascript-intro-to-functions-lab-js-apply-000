a644318e62fba52de18fdb87efc3b56cfde28ff7
Philip Hazeley <ayodelehazeley@gmail.com>
2018-01-22 15:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function shout (string) {
   return string.toUpperCase()
 }
+function whisper (string){
+  return string.toUpperCase()
+}
+function describe (string){
+  return string.toUpperCase()
+}
