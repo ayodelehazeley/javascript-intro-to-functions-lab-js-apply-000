e63a821f61b1129a475e3b405308ee1a1ed82607
Philip Hazeley <ayodelehazeley@gmail.com>
2018-01-22 15:24:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-function shout (string) {
+function shout(string) {
   return string.toUpperCase()
 }
-function whisper (string){
+
+function whisper(string) {
   return string.toLowerCase()
 }
-function describe (string){
-  return string.toUpperCase()
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
-function LogShout (string){
-  console.log (uppercase)
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
